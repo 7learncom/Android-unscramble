51b5e083292ba8d85537a6eea439a54f0b991332
Mohammad Hasan Dabbaghy <m.dabbaghy@gmail.com>
2024-01-03 10:22:09 +0330

    Finish game

---
+++ app/src/main/java/ir/mhd/unscramble/ui/game/GameFragment.kt
@@ -0,0 +1,132 @@
+package ir.mhd.myapplication.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import ir.mhd.myapplication.R
+import ir.mhd.myapplication.databinding.FragmentGameBinding
+import kotlinx.coroutines.launch
+
+class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding!!
+
+    private val viewModel: GameViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                launch {
+                    viewModel.isGameOver.collect {
+                        if (it) {
+                            showFinalScoreDialog()
+                        }
+                    }
+                }
+
+                launch {
+                    viewModel.currentScrambledWord.collect {
+                        binding.textViewUnscrambledWord.text = it
+                    }
+                }
+
+                launch {
+                    viewModel.currentWordCount.collect {
+                        binding.wordCount.text = getString(R.string.word_count, it, MAX_NO_OF_WORDS)
+                    }
+                }
+
+                launch {
+                    viewModel.score.collect {
+                        binding.score.text = getString(R.string.score, it)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onSubmitWord() {
+        val playerWord = binding.textInputEditText.text.toString()
+
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+            if (!viewModel.nextWord()) {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
+        }
+    }
+
+    private fun onSkipWord() {
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+        } else {
+            showFinalScoreDialog()
+        }
+    }
+
+    /*
+     * Creates and shows an AlertDialog with final score.
+     */
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                exitGame()
+            }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                restartGame()
+            }
+            .show()
+    }
+
+    private fun restartGame() {
+        viewModel.reinitializeData()
+        setErrorTextField(false)
+    }
+
+    private fun exitGame() {
+        activity?.finish()
+    }
+
+    private fun setErrorTextField(error: Boolean) {
+        if (error) {
+            binding.textField.isErrorEnabled = true
+            binding.textField.error = getString(R.string.try_again)
+        } else {
+            binding.textField.isErrorEnabled = false
+            binding.textInputEditText.text = null
+        }
+    }
+}
---
+++ app/src/main/java/ir/mhd/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,91 @@
+package ir.mhd.myapplication.ui.game
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class GameViewModel : ViewModel() {
+
+    private val _isGameOver = MutableStateFlow(false)
+    val isGameOver = _isGameOver.asStateFlow()
+
+    private val _score = MutableStateFlow(0)
+    val score: StateFlow<Int> = _score.asStateFlow()
+
+    private val _currentWordCount = MutableStateFlow(0)
+    val currentWordCount: StateFlow<Int> = _currentWordCount.asStateFlow()
+
+    private val _currentScrambledWord = MutableStateFlow<String?>(null)
+    val currentScrambledWord = _currentScrambledWord.asStateFlow()
+
+    private val wordsList: MutableList<String> = mutableListOf()
+
+    private lateinit var currentWord: String
+
+
+    init {
+        getNextWord()
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        }
+
+        val tempWord = currentWord.toCharArray()
+
+        do {
+            tempWord.shuffle()
+        } while (String(tempWord).equals(currentWord, false))
+
+        Log.d("Unscramble", "currentWord= $currentWord")
+        _currentScrambledWord.value = String(tempWord)
+        _currentWordCount.update {
+            it + 1
+        }
+        wordsList.add(currentWord)
+    }
+
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+        _isGameOver.value = false
+    }
+
+    private fun increaseScore() {
+        _score.update {
+            it + SCORE_INCREASE
+        }
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else {
+            _isGameOver.value = true
+            false
+        }
+    }
+}
